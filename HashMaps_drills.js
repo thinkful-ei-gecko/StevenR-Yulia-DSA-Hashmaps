65301dccdca7b7dbacae2240dad049849a0f5f95
stevenr1 <stevenr.thinkful@gmail.com>
2019-11-11 12:56:56 -0600

    updates

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -3,8 +3,8 @@ const HashMap = require('./HashMap');
 function main() {
   const lotr = new HashMap;
 
-  lotr.MAX_LOAD_RATIO = 0.5;
-  lotr.SIZE_RATIO = 3;
+  HashMap.MAX_LOAD_RATIO = 0.5;
+  HashMap.SIZE_RATIO = 3;
 
   lotr.set('Hobbit', 'Bilbo');
   lotr.set('Hobbit', 'Frodo');
@@ -27,4 +27,25 @@ function main() {
 //Since Frodo was inserted after Bilbo key 'Hobbit' corresponds 'Frodo'
 //Same for key 'Maiar', value The Necromancer became 'Sauron'
 
-console.log(main());
+// console.log(main());
+
+// 2. This code produces 2 values (20, 10) while only having 1 key value.
+
+const WhatDoesThisDo = function(){
+  let str1 = 'Hello World.'; // assign string value 1 to Hello World
+  let str2 = 'Hello World.'; // assign string value 2 to Hello World
+  let map1 = new HashMap(); // creating new instance of HashMap
+  map1.set(str1,10); // create key value where Hello World is the key and 10 is the value
+  map1.set(str2,20); // rewrite value to 20
+  let map2 = new HashMap(); // create new instance for HashMap
+  let str3 = str1; // string 3 has Hello World as value
+  let str4 = str2; // string 4 has Hello World as value
+  map2.set(str3,20); // key is Hello World and value is 20
+  map2.set(str4,10); // reassign key Hello World to value 10
+
+  console.log(map1.get(str1));
+  console.log(map2.get(str3));
+}
+console.log(WhatDoesThisDo())
+
+// 3. Our hashmap would look like 22, 10 88, empty, 4, 15, 28, 17, 59, 31 empty
